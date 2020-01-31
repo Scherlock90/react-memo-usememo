dbab810f9bb242cf8ca9617ce20c8824837c947a
Sebastian Napora <sebastian.napora@intive.com>
2020-01-31 13:56:37 +0100

    Updates

---
+++ precache-manifest.233e05e585dc59c2123263071d8b2ee5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca213db02e8e516cd156d251e0c0a7ff",
+    "url": "/react-memo-usememo/index.html"
+  },
+  {
+    "revision": "8e6e5c802549bad6a329",
+    "url": "/react-memo-usememo/static/css/main.59320b99.chunk.css"
+  },
+  {
+    "revision": "3623620cd21da990c854",
+    "url": "/react-memo-usememo/static/js/2.623d0d1f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-memo-usememo/static/js/2.623d0d1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e6e5c802549bad6a329",
+    "url": "/react-memo-usememo/static/js/main.387355b6.chunk.js"
+  },
+  {
+    "revision": "a7a53bf49055fbb7d875",
+    "url": "/react-memo-usememo/static/js/runtime-main.87ffe22a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-memo-usememo/static/media/logo.5d5d9eef.svg"
+  }
+]);
